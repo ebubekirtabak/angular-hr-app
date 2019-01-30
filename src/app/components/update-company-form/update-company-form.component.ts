e1488cd8b599c3ab9c22b74baddeba697b35c263
Ebubekir Tabak <ebubekir.tabak@yahoo.com>
2019-01-30 23:18:49 +0300

    Added company form component.

---
+++ src/app/components/update-company-form/update-company-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ApiService } from '@services/api.service';
+import { NgForm } from '@angular/forms';
+import { Company } from '@models/company.model';
+
+
+@Component({
+  selector: 'app-update-company-form-component',
+  templateUrl: './update-company-form.component.html',
+  styleUrls: ['./update-company-form.component.scss']
+})
+export class UpdateCompanyFormComponent implements OnInit {
+
+  @Input() company: Company;
+  public isFormSubmitted: boolean;
+  constructor (
+    private apiService: ApiService
+  ) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  onSubmit(form: NgForm) {
+    this.isFormSubmitted = true;
+    if (form.valid) {
+      // this.formSubmit.emit({ isValid: true, formElements: form.value });
+    }
+  }
+
+}
